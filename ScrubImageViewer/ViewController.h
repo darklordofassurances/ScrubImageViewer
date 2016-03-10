19519f4615c24fdd7819d836c5f7e76b06f6b772
Andrew Rohn <atrohn@gmail.com>
2016-03-09 22:07:03 -0600

    ViewController handles updating of images as UIPanGestureRecognizer detects user pans horizontally.

--- ScrubImageViewer/ViewController.h
+++ ScrubImageViewer/ViewController.h
@@ -1,10 +1,3 @@
-//
-//  ViewController.h
-//  ScrubImageViewer
-//
-//  Created by Andrew Rohn on 3/9/16.
-//  Copyright © 2016 Andrew Rohn. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- ScrubImageViewer/ViewController.m
+++ ScrubImageViewer/ViewController.m
@@ -1,27 +1,89 @@
-//
-//  ViewController.m
-//  ScrubImageViewer
-//
-//  Created by Andrew Rohn on 3/9/16.
-//  Copyright © 2016 Andrew Rohn. All rights reserved.
-//
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSMutableArray<UIImage *> *images;
+@property (weak, nonatomic) IBOutlet UIImageView *scrubImageView;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.scrubImageView.image = [self.images objectAtIndex:0];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/*
+ Keep track of users panning and update image as appropriate.
+ */
+- (IBAction)scrubImageViewPan:(UIPanGestureRecognizer *)sender
+{
+    static CGFloat currentX = 0;
+    if (sender.state == UIGestureRecognizerStateChanged ||
+        sender.state == UIGestureRecognizerStateEnded) {
+        //Distance the gesture has moved
+        CGPoint translation = [sender translationInView:self.scrubImageView];
+        currentX += translation.x;
+        //Reset the translaton point
+        [sender setTranslation:CGPointZero inView:self.scrubImageView];
+        
+        CGFloat maxWidth = self.scrubImageView.frame.size.width * .6;
+        if (currentX > maxWidth) {
+            currentX = maxWidth;
+        }
+        else if (currentX < 0) {
+            currentX = 0;
+        }
+        
+        self.scrubImageView.image = [self.images objectAtIndex:[self indexForPosition:currentX andMaxWidth:maxWidth]];
+    }
+}
+
+/*
+ Return the index of the image in images.
+ @warning The number of scrub images cannot be greater than maxWidth
+ @param currentX The current horizontal position the user has panned to
+ @param maxWidth The maximum width the horizontal position can be for currentX
+ */
+- (NSUInteger)indexForPosition:(CGFloat)currentX andMaxWidth:(CGFloat)maxWidth
+{
+    float divisor = maxWidth / self.images.count;
+    NSUInteger index = currentX / divisor;
+    
+    //Prevent index out of bounds error
+    if (index >= self.images.count) {
+        index = self.images.count-1;
+    }
+    
+    return index;
+}
+
+- (NSMutableArray *)images
+{
+    if (!_images) {
+        _images = [[NSMutableArray alloc] init];
+        [self loadImages];
+    }
+    return _images;
+}
+
+- (void)loadImages
+{
+    NSUInteger numberOfFrames = 44;
+    for (NSUInteger i = 1; i <= numberOfFrames; ++i) {
+        NSString *imageName = [NSString stringWithFormat:@"frame%lu", (unsigned long)i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        if (image) {
+            [self.images addObject:image];
+        }
+        else {
+            NSLog(@"Could not load image named: %@", imageName);
+        }
+    }
 }
 
 @end
